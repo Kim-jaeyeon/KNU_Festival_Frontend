020c01171b7beb0be3685ebaf5114ae0937b2df4
kpss0337 <kpss0337@gmail.com>
2025-08-28 23:59:53 +0900

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "tailwindcss";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-red-500 text-3xl font-bold underline">Hello Tailwind + TS!</h1>
+    </div>
+  )
+}
+export default App
