707828edff251d27d7142533467de39dd4bf211d
kpss0337 <kpss0337@gmail.com>
2025-09-18 07:41:03 +0900

    test41

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -16,14 +16,10 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
   if (!open) return null;
 
   const handleKakaoLogin = () => {
-    if (!nickname) {
-      toast.error("닉네임은 필수 입력값입니다.");
-      return;
-    }
-    if (!phone) {
-      toast.error("전화번호는 필수 입력값입니다.");
-      return;
-    }
+    if (nickname) sessionStorage.setItem("nickname", nickname);
+  if (phone) sessionStorage.setItem("phone", phone);
+
+
 
     sessionStorage.setItem("nickname", nickname);
     sessionStorage.setItem("phone", phone);
@@ -37,7 +33,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
       <div
         className="relative w-[279px] h-[434px] rounded-[15px] border border-gray-300 bg-[#EEF2DAE5] shadow-lg flex flex-col"
         onClick={(e) => e.stopPropagation()}
-        style={{ paddingTop: "90px", paddingLeft: "35px", paddingRight: "35px", paddingBottom: "75px" }}
+        style={{ paddingTop: "90px", paddingLeft: "35px", paddingRight: "35px", paddingBottom: "25px" }}
       >
         <button onClick={onClose} className="absolute top-3 right-3 text-lg font-bold text-[rgba(68,98,64,0.60)] hover:text-[#446240]">✕</button>
         <h2 className="absolute top-6 left-1/2 -translate-x-1/2 font-pretendard font-semibold text-[20px] leading-[32px] text-black">
@@ -62,12 +58,17 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
           className="flex h-[48px] w-full px-5 py-4 rounded-[20px] border border-gray-300 bg-[rgba(255,255,255,0.54)] text-[15px] font-normal text-[#285100] focus:outline-none focus:ring-2 focus:ring-[#A7C957] mb-6"
         />
 
+        <div className="text-center text-black text-sm mb-4">
+          처음 로그인시에만 <br />
+          닉네임과 전화번호 기입
+        </div>
         <button
           onClick={handleKakaoLogin}
           className="flex w-[209px] h-[48px] justify-center items-center rounded-[20px] bg-[#F6E550] font-pretendard font-bold text-[15px] text-black shadow"
         >
           카카오계정 로그인
         </button>
+        
       </div>
     </div>
   );
