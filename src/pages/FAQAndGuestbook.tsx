2400373f73f0fe881ffb16a2b3670e69ffd896d1
kpss0337 <kpss0337@gmail.com>
2025-09-17 23:24:43 +0900

    test8

--- src/pages/FAQAndGuestbook.tsx
+++ src/pages/FAQAndGuestbook.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
 import TabNav from "../components/TabNav";
 import FAQItem from "../components/FAQItem";
 import api from "../utils/api";
